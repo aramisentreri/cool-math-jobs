cdb799fbd20fa9c1bb6a480259f430a4513ee60d
Gustavo <20483465+aramisentreri@users.noreply.github.com>
2024-08-06 23:00:22 -0700

    Added a database and better design using Cards.

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from fasthtml.common import *
-from fasthtml import *
+from fasthtml import common as fh
 from utility_components import Hero, job_list, job_row
 from get_jobs_from_google_sheets import get_jobs_from_google_sheets
 
-css = Style(':root { --pico-font-size: 100%; --pico-font-family: Pacifico, cursive;}')
-app = FastHTML(hdrs=(picolink, css))
+css = fh.Style(':root { --pico-font-size: 100%; --pico-font-family: Pacifico, cursive;}')
+
+def render(job):
+    return fh.Card(fh.Li(f"{job.title} | {job.description} | {job.salary_range} | {job.why_is_cool} | ", fh.A('link', href={job.link})))
+
+app, rt, jobs, Job = fh.fast_app('jobs.db', hdrs=(fh.picolink, css), live=True, render=render,
+                        id=int,
+                        title=str, 
+                        description=str, 
+                        link=str,
+                        salary_range=str,
+                        why_is_cool=str,
+                        email=str,
+                        pk='id'
+                        )
 
 submitted_jobs = []
 
@@ -12,46 +24,68 @@ from dataclasses import dataclass
 import pandas as pd 
 import numpy as np 
 
-    
 
 count = 0
-# @rt("/")
-@app.get("/")
-def home():
+@rt("/")
+def get():
     # # Generate random data
     # data = np.random.rand(5, 4)  # 5 rows and 4 columns
     # # Create a DataFrame with the random data
     # df = pd.DataFrame(data, columns=['A', 'B', 'C', 'D'])
-    jobs = get_jobs_from_google_sheets()
-    return Title("Cool Math Jobs"), Main(
-        Titled("",
+    # jobs.insert(Job(title="Test job", 
+    #                 description="cool job",
+    #                 link="google.com",
+    #                 salary_range="1-2 USD",
+    #                 why_is_cool="because",
+    #                 email="me@me.com" ))
+    
+    # return fh.Titled('Jobs',
+    #               fh.Ul(*jobs()),
+    #             )
+    return fh.Title("Cool Math Jobs"), fh.Main(
+        fh.Titled("",
             Hero("Cool Math Jobs", "Find a fun job for your brain"),
-            Div(job_list(jobs)),
-            P(f"Count is set to {count}", id="count"),
-            Button("Increment", hx_post="/increment", hx_target="#count", hx_swap="innerHTML"),
-            A("Link to submission page (to add jobs)", href="/submit_job"),
+            fh.P(fh.A(fh.Button("Submit a job"), href="/submit_job")),
+            fh.Card(fh.Ul(*jobs())),
+            # fh.P(f"Count is set to {count}", id="count"),
+            # fh.Button("Submit a job", hx_get="/submit_job", hx_target="#count", hx_swap="innerHTML"),
+            
             # Div(NotStr(df.to_html())) 
         ), 
         cls="container"
     )
 
-@app.get("/submit_job")
-def submit_job():
-    return Main(P("Submit a cool math job for consideration on the board:"),
-                Form(Input(type="text", name="data"),
-                     Button("Submit"),
-                     action="/", method="post"))
-@app.post("/")
-def add_submitted_job(data:str):
-    print(data)
-    # TODO: Send an email with the details of the job to see if it's worthy
-    return home()
+# @app.get("/submit_job")
+@rt("/submit_job")
+def get():
+    frm = fh.Form(
+        fh.Group(
+            fh.Input(placeholder="job title", name='title'), 
+            fh.Input(placeholder="description", name='description'), 
+            fh.Input(placeholder="link", name='link'), 
+            fh.Input(placeholder="salary range", name='salary_range'),
+            fh.Input(placeholder="why is it cool", name='why_is_cool'),  
+            fh.Input(placeholder="email", name='email'), 
+            fh.Button("Submit")
+            ), 
+            hx_post='/submit_job'
+        )
+    return fh.Titled("Submit job", 
+                fh.Main(fh.P("Submit a cool math job for consideration on the board:"),
+                    fh.Card(fh.A(fh.Button("Go back to jobs"), href="/"), header=frm),
+                )
+            )
+@rt("/submit_job")
+def post(job:Job):
+    # TODO: Add logic here to send me an email instead. 
+    return jobs.insert(job)
 
-@app.post("/increment")
-def increment():
+
+@rt("/increment")
+def post():
     print("incrementing")
     global count
     count += 1
     return f"Count is set to {count}"
 
-serve()
+fh.serve()
